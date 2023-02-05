70b0a743c75d31c39af3f081b192fb6f360b06ae
AMG <53061696+alexisMoragaa@users.noreply.github.com>
2023-02-05 19:07:30 -0300

    Se añade una funcion llamada clickHandlerFucntion en el componente calculadora, la cual es enviada al componente button, el cual la ejecuta cuando se desencadena el evento onclick  y pasa como parametro el texto del boton sobre el cual se desencadeno el evento

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 import PropTypes from 'prop-types'
 
+
+
+/**
+ * Se añade la funcion onclick y esta a su ves ejecuta la funcion handler, usando el valor text del boton como parametro
+ * la funcion handler es recibida desde el componente padre y envia de regreso el valor obtenido del boton presionado
+ */
 const Button = ({text, type, handler}) => {
     return (
-            <button className={type}>
+            <button className={type} onClick={() => {handler(text)}}>
                 <span>{text}</span>
             </button>
     )
--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -6,23 +6,27 @@ import Button from './Button'//Importamos el componente de los botones
 import '../css/calculadora.css'
 
 
+const clickHandlerFucntion = (text) => {
+    console.log("El valor recibido desde el componente button es : "+text)
+}
+
 const Calculadora = () => {
     return (
         <main className="react-calculator">
-           <Result vaue={12}/>
+           <Result value={12}/>
            <div className="numbers">
-            <Button text={"9"}/>
-            <Button text={"8"}/>
-            <Button text={"7"}/>
-            <Button text={"6"}/>
-            <Button text={"5"}/>
-            <Button text={"4"}/>
-            <Button text={"3"}/>
-            <Button text={"2"}/>
-            <Button text={"1"}/>
-            <Button text={"0"}/>
+            <Button text={"9"} handler={clickHandlerFucntion}/>
+            <Button text={"8"} handler={clickHandlerFucntion}/>
+            <Button text={"7"} handler={clickHandlerFucntion}/>
+            <Button text={"6"} handler={clickHandlerFucntion}/>
+            <Button text={"5"} handler={clickHandlerFucntion}/>
+            <Button text={"4"} handler={clickHandlerFucntion}/>
+            <Button text={"3"} handler={clickHandlerFucntion}/>
+            <Button text={"2"} handler={clickHandlerFucntion}/>
+            <Button text={"1"} handler={clickHandlerFucntion}/>
+            <Button text={"0"} handler={clickHandlerFucntion}/>
            </div>
-           {/* <NumerosCalculadora /> */}
+           
         </main>
     )
 }
