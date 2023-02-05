c8791302326eaa7a3b4f7a4992ef3df38c8d954b
AMG <53061696+alexisMoragaa@users.noreply.github.com>
2023-02-05 17:42:55 -0300

    Se añade Destructurin y una explicacion de lo que esto es en el componente Result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
 
-const Result = (props) => {
+/** Destrucuring
+ * El destructuring es una forma de descomponer un objeto y separar sus valores en variables independientes
+ * Para esto  abrimos llaves, dentro de las llaves escribimos las variables que deseamos extraer del objeto
+ * y finalmente igualamos esto al valor del objeto que queremos deconstuir 
+ * EJ: const { valor1, valor2 } = objeto_inicial
+ * Siguiendo el ejemplo anterior estamos extrayendo las variables valor1 y valor2, las cuales deben estar dentro del objeto
+ * con este metodo logramos extraer el valor a variables normales las cuales podemos manipular de forma independiente
+ * Otra forma de hacerlo es instanciarlo directamente en la recepcion de parametros de un componente, 
+ * como se muestra en el componente Result
+ */
+
+const Result = ({num1, num2}) => {
     
     return(
         <div className="result">
-            <span>{props.num1 + props.num2}</span>
+            <span>{num1 + num2}</span>
         </div>
     )
 }
