4bad1c845d48e2bfb0db2e7f2d710d35253ced25
AMG <53061696+alexisMoragaa@users.noreply.github.com>
2023-02-10 12:08:11 -0300

    Prueba

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -18,7 +18,7 @@ const Result = ({value}) => {
     
     return(
         <div className="result">
-            <span>{value} Prueba </span>
+            <span>{value}</span>
         </div>
     )
 }
