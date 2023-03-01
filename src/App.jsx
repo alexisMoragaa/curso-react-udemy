3db40e9706a039621846d2d5a5b5952e7ef07825
AMG <53061696+alexisMoragaa@users.noreply.github.com>
2023-02-28 23:59:02 -0300

    Se añaden links al Router del componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Calculadora from './components/Calculadora'
 
 const App = props => {
   return (
 
         <Router>
-            <Routes>
+
+            <div> {/* Establece los links como barra de navegacion*/} 
+                <Link to="/">Home</Link>
+                <Link to="/calculadora">Calculadora</Link>
+            </div>
+
+            <Routes> {/* Renderiza los componentes correspondientes cuando la ruta se coincide con la indicada en el path */}
                 <Route path='/' element={"Home"}/>
                 <Route path='/main' element={"Main"}/>
                 <Route path='/calculadora'element={<Calculadora />}/>
