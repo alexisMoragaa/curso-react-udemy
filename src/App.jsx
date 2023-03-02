ecb032554f11708eab68010291cf435454d0d1fa
AMG <53061696+alexisMoragaa@users.noreply.github.com>
2023-03-01 22:57:17 -0300

    Se añade contenedor con grid al componente app que esta renderizando el router

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Grid } from '@mui/material'
 import { CalculadoraPage } from './pages/CalculadoraPage'
 import { NotFoundPages } from './pages/NotFoundPages'
 import { WelcomePage } from './pages/WelcomePage'
 const App = props => {
   return (
 
-        <Router>
+        <Grid container justifyContent="center" direction="row">
+            <Grid item sm={11}>
 
-            <div> {/* Establece los links como barra de navegacion*/} 
-                <Link to="/">Home</Link>
-                <Link to="/calculadora">Calculadora</Link>
-            </div>
+                <Router>
 
-            <Routes> {/* Renderiza los componentes correspondientes cuando la ruta se coincide con la indicada en el path */}
-                <Route path='/' element={<WelcomePage />}/>
-                <Route path='/cities' element={"Main"}/>
-                <Route path='/calculadora'element={<CalculadoraPage />}/>
-                <Route path='*' element={<NotFoundPages />}/>
-            </Routes>
-        </Router>
+                    <div> {/* Establece los links como barra de navegacion*/} 
+                        <Link to="/">Home</Link>
+                        <Link to="/calculadora">Calculadora</Link>
+                    </div>
+
+                    <Routes> {/* Renderiza los componentes correspondientes cuando la ruta se coincide con la indicada en el path */}
+                        <Route path='/' element={<WelcomePage />}/>
+                        <Route path='/cities' element={"Main"}/>
+                        <Route path='/calculadora'element={<CalculadoraPage />}/>
+                        <Route path='*' element={<NotFoundPages />}/>
+                    </Routes>
+                </Router>
+
+            </Grid>
+        </Grid>    
  
   )
 }
